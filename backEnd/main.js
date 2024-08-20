df0949e6c81d9fe3c66eb5b52ad8f4aa4dc5019e
murtadar10 <murtadar13@gmail.com>
2024-08-20 19:12:09 +0300

    for mobile002

--- backEnd/main.js
+++ backEnd/main.js
@@ -1,3 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebar = document.getElementById('sidebar');
+  
+  function toggleSidebar() {
+    sidebar.classList.toggle('expanded');
+  }
+
+  function closeSidebarOnClickOutside(event) {
+    if (!sidebar.contains(event.target)) {
+      sidebar.classList.remove('expanded');
+    }
+  }
+
+  sidebar.addEventListener('click', toggleSidebar);
+
+  document.addEventListener('click', closeSidebarOnClickOutside);
+});
+
+const classes = ['D4pow-secondary', 'D4pow', 'element', 'numbersOfCalcZn', 'powersOfCalcZn'];
+
+classes.forEach(className => {
+    document.querySelectorAll(`.${className}`).forEach(function(inputElement) {
+        inputElement.addEventListener('focus', function(e) {
+            e.target.blur(); // إلغاء التركيز على الحقل
+        });
+    });
+});
 
 
 function showSection(sectionId) {
